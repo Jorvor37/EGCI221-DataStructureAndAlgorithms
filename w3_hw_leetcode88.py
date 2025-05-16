ebad6a6574d428c1f214884800cef659d62c798c
JorVor <129814086+Jorvor37@users.noreply.github.com>
2025-05-16 08:05:36 +0000

    syntax error fix

--- w3_hw_leetcode88.py
+++ w3_hw_leetcode88.py
@@ -1,25 +1,23 @@
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        def merge(nums1, m, nums2, n):
-            p1 = m - 1
-            p2 = n - 1
-            p = m + n - 1
+from typing import List
 
-            # Merge from the back
-            while p1 >= 0 and p2 >= 0:
-                if nums1[p1] > nums2[p2]:
-                    nums1[p] = nums1[p1]
-                    p1 -= 1
-                else:
-                    nums1[p] = nums2[p2]
-                    p2 -= 1
-                p -= 1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
 
-            # Copy remaining elements from nums2 (if any)
-            while p2 >= 0:
+        # Merge from the back
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
-                p -= 1
-            }
-};
+            p -= 1
+
+        # Copy remaining elements from nums2 (if any)
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
